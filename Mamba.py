61ea8678303cd2abffb79b0a2b992c61f6220207
SeungWoo Jeong <wjdtmddn2030@gmail.com>
2025-09-17 18:36:25 +0900

    Add files via upload

---
+++ Mamba.py
@@ -0,0 +1,101 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from einops import rearrange
+
+from causal_conv1d import causal_conv1d_fn
+from SSM import PI2DSSM
+
+from fft import dft, idft
+
+class RMSNorm(nn.Module):
+    def __init__(self, dim: int, eps: float = 1e-5):
+        super().__init__()
+        self.eps = eps
+        self.weight = nn.Parameter(torch.ones(dim))
+    def forward(self, x):
+        norm_x = x * torch.rsqrt(x.pow(2).mean(-1, keepdim=True) + self.eps)
+        return self.weight * norm_x
+
+class PIMamba(nn.Module):
+    def __init__(self, d_model, state_size, variable_dim, expand=2, d_conv=4, dropout=0.1):
+        super().__init__()
+        self.d_model = d_model        # Hidden dim
+        self.d_inner = d_model * expand 
+        self.state_size = state_size
+        self.variable_dim = variable_dim
+        self.d_conv = d_conv
+
+        self.norm_in = RMSNorm(self.d_model) # B, C, L
+        self.in_proj = nn.Linear(self.d_model, self.d_inner, bias=False)
+
+
+        n_variable = self.variable_dim
+        self.conv_weight = nn.Parameter(torch.empty(n_variable, d_conv))
+        self.conv_bias = nn.Parameter(torch.zeros(n_variable))
+        nn.init.kaiming_normal_(self.conv_weight)
+        nn.init.zeros_(self.conv_bias)
+
+        # Multi-Scale branches
+        self.long_branch = PI2DSSM(n_variable, state_size, dt_min=0.1, dt_max=0.5) # [0.001, 0.1]
+        self.short_branch = PI2DSSM(n_variable, state_size, dt_min=0.01, dt_max=0.05) # [0.001, 0.05]
+        self.freq_branch = PI2DSSM(n_variable, state_size, dt_min=0.001, dt_max=0.01) # [0.001, 0.01]
+
+        self.norm_long = nn.LayerNorm(self.d_inner)
+        self.norm_short = nn.LayerNorm(self.d_inner)
+        self.norm_freq = nn.LayerNorm(self.d_inner)
+
+        self.alpha_long = nn.Parameter(torch.tensor(0.2))
+        self.alpha_short = nn.Parameter(torch.tensor(0.2))
+        self.alpha_freq = nn.Parameter(torch.tensor(0.2))
+
+        self.gate_fc = nn.Sequential(
+            nn.Linear(3 * self.d_inner, 3),
+            nn.Softmax(dim=-1)
+        )
+        self.out_proj = nn.Linear(self.d_inner, self.d_model, bias=False)
+        self.norm_out = nn.LayerNorm(self.d_model)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        # x: [B, C, L]
+        B, C, L = x.shape
+        residual = x
+        # RMSNorm & input projection
+        x = self.norm_in(x)           # [B, C, L]
+        x = self.in_proj(x) # [B, C, L]
+
+        # causal conv1d: (B, C, L)
+        x_conv = causal_conv1d_fn(
+            x, self.conv_weight, self.conv_bias, activation='silu'
+        )
+
+        x_conv_freq = dft(x_conv.permute(0, 2, 1)).permute(0, 2, 1)
+
+        # Multi-Scale branches
+        x_branches = torch.cat([x_conv, x_conv, x_conv_freq], dim=0)  # [3B, C, L]
+        branch_outputs = []
+
+        for branch, x_in in zip([self.long_branch, self.short_branch, self.freq_branch],
+                                torch.chunk(x_branches, 3, dim=0)):
+            branch_outputs.append(branch(x_in))
+
+        y_long, y_short, y_freq = branch_outputs
+
+        y_long = self.alpha_long*self.norm_long(y_long) + x_conv
+        y_short = self.alpha_short*self.norm_short(y_short) + x_conv
+        y_freq = self.alpha_freq*self.norm_freq(y_freq) + x_conv_freq
+
+
+        pooled_long = y_long.mean(dim=1)  # [B, L]
+        pooled_short = y_short.mean(dim=1)
+        pooled_freq = y_freq.mean(dim=1)
+
+        gate_in = torch.cat([pooled_long, pooled_short, pooled_freq], dim=-1)  # [B, 3*L]
+        gate = self.gate_fc(gate_in)                             # [B, 3]
+        gate = gate.unsqueeze(-1).unsqueeze(-1)    # [B, 3, 1, 1]
+        y_stack = torch.stack([y_long, y_short, y_freq], dim=1)          # [B, 3, C, L]
+        out = (gate * y_stack).sum(dim=1)                        # [B, C, L]
+
+        out = self.dropout(self.norm_out(self.out_proj(out) + residual))
+        return out
---
+++ SSM.py
@@ -0,0 +1,169 @@
+import torch
+import torch.nn as nn
+import math
+from einops import rearrange
+from mamba_ssm.ops.selective_scan_interface import selective_scan_fn
+
+class PI2DSSM(nn.Module):
+    def __init__(
+        self, d_inner: int, state_size: int,
+        dt_rank: str | int = "auto",
+        dt_min: float = 0.001, dt_max: float = 0.1, dt_init: str = "random",
+        dt_scale: float = 1.0, dt_init_floor: float = 1e-4, bias: bool = False,
+        device: str = 'cuda', dtype: torch.dtype = torch.float32, pool_type='sum'
+    ):
+        super().__init__()
+        self.d_inner = d_inner
+        self.state_size = state_size
+        self.pool_type = pool_type
+
+        self.dt_rank = math.ceil(d_inner / 16) if dt_rank == "auto" else dt_rank
+        factory_kwargs = {"device": device, "dtype": dtype}
+
+        # SSM parameters (horizontal)
+        self.x_proj_hh = nn.Linear(d_inner, self.dt_rank + state_size * 2, bias=bias, **factory_kwargs)
+        self.dt_proj_hh = nn.Linear(self.dt_rank, d_inner, bias=True, **factory_kwargs)
+        A_hh = torch.arange(1, state_size + 1, dtype=torch.float32, device=device).repeat(d_inner, 1)
+        self.A_log_hh = nn.Parameter(torch.log(A_hh))
+        self.D_hh = nn.Parameter(torch.ones(d_inner, device=device))
+
+        # SSM parameters (vertical)
+        self.x_proj_hv = nn.Linear(d_inner, self.dt_rank + state_size * 2, bias=bias, **factory_kwargs)
+        self.dt_proj_hv = nn.Linear(self.dt_rank, d_inner, bias=True, **factory_kwargs)
+        A_hv = torch.arange(1, state_size + 1, dtype=torch.float32, device=device).repeat(d_inner, 1)
+        self.A_log_hv = nn.Parameter(torch.log(A_hv))
+        self.D_hv = nn.Parameter(torch.ones(d_inner, device=device))
+
+        # Bias init
+        for dt_proj in [self.dt_proj_hh, self.dt_proj_hv]:
+            dt_init_std = self.dt_rank**-0.5 * dt_scale
+            if dt_init == "constant":
+                nn.init.constant_(dt_proj.weight, dt_init_std)
+            elif dt_init == "random":
+                nn.init.uniform_(dt_proj.weight, -dt_init_std, dt_init_std)
+            else:
+                raise NotImplementedError(f"Invalid dt_init: {dt_init}")
+            dt = torch.exp(torch.rand(d_inner, **factory_kwargs) * (math.log(dt_max) - math.log(dt_min)) + math.log(dt_min)).clamp(min=dt_init_floor)
+            inv_dt = dt + torch.log(-torch.expm1(-dt))
+            with torch.no_grad():
+                dt_proj.bias.copy_(inv_dt)
+            dt_proj.bias._no_reinit = True
+
+        # summary
+        self.W_v = nn.Linear(1, d_inner, bias=False)
+        self.Psi_proj_hh = nn.Linear(d_inner, state_size, bias=False)
+        self.Psi_proj_hv = nn.Linear(d_inner, state_size, bias=False)
+        self.X_proj_hv = nn.Linear(d_inner, state_size, bias=False)
+
+        # attention aggregation
+        if self.pool_type == 'attention': 
+            self.att_dim = min(d_inner, 64)
+            self.W_q = nn.Linear(d_inner, self.att_dim, bias=False)
+            self.W_k = nn.Parameter
+            self.var_key = nn.Parameter(torch.randn(d_inner, self.attn_dim) / math.sqrt(self.attn_dim))
+            self.var_value = nn.Parameter(torch.randn(d_inner, d_inner) / math.sqrt(d_inner)) 
+
+        # output proj
+        self.C_h = nn.Linear(d_inner, d_inner, bias=False)
+        self.C_v = nn.Linear(d_inner, d_inner, bias=False)
+
+    def aggregation(self, h_v, pool_type='mean'):
+        # h_v: [B, C, L]
+        # z = h_v.sum(dim=1, keepdim=True) / h_v.shape[1] # [B, 1, L]
+        # proj = self.W_v(z.permute(0, 2, 1)).permute(0, 2, 1)
+        # return proj
+        if self.pool_type == 'mean':
+            z = h_v.mean(dim=1, keepdim=True)
+            proj = self.W_v(z.permute(0, 2, 1)).permute(0, 2, 1)
+        elif self.pool_type == 'sum':
+            z = h_v.sum(dim=1, keepdim=True) / h_v.shape[1] # scaling
+            proj = self.W_v(z.permute(0, 2, 1)).permute(0, 2, 1)
+        elif self.pool_type == 'attention': 
+            h = h_v.transpose(-1, -2) # [B, L, C]
+            q = self.W_q(h) # [B, L, att_dim]
+            logits = torch.einsum("blh,ch->blc", q, self.var_key) / math.sqrt(self.att_dim)
+            alpha = torch.softmax(logits, dim=-1)
+            w= alpha * h
+            proj = torch.einsum("blc,cd->bld", w, self.var_value).transpose(-1, -2)
+        else:
+            raise NotImplementedError("pooling error")
+        return proj
+
+    def selective_scan_with_psi(self, u, psi_seq, dt, A, B, C, D, psi_proj, delta_bias=None, x_proj=None, x_extra=None):
+        psi_B = psi_proj(psi_seq.permute(0, 2, 1)).permute(0, 2, 1)  # [B*, N, L]
+        B_mod = B + psi_B
+        C_mod = C 
+
+        if x_proj is not None and x_extra is not None:
+            x_B = x_proj(x_extra.permute(0, 2, 1)).permute(0, 2, 1)  # [B*, N, L]
+            B_mod = B_mod + x_B
+            C_mod = C_mod + x_B
+
+        y = selective_scan_fn(
+            u=u, delta=dt, A=A, B=B_mod, C=C_mod, D=D, z=None,
+            delta_bias=delta_bias, delta_softplus=True,
+        )
+        return y
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        x: [B, C, L]
+        return: [B, C, L]
+        """
+        B, C, L = x.shape
+
+        psi_seq = self.aggregation(x)  # [B, L]
+
+        # h_h
+        x_reshaped = rearrange(x, "b c l -> (b l) c")
+        x_proj_out_hh = self.x_proj_hh(x_reshaped)
+        dt_inter_hh, B_hh, C_hh = torch.split(x_proj_out_hh, [self.dt_rank, self.state_size, self.state_size], dim=-1)
+        dt_hh = self.dt_proj_hh.weight @ dt_inter_hh.t()
+        dt_hh = rearrange(dt_hh, "c (b l) -> b c l", l=L)
+        A_hh = -torch.exp(self.A_log_hh.float())
+        B_proj_hh = rearrange(B_hh, "(b l) n -> b n l", l=L)
+        C_proj_hh = rearrange(C_hh, "(b l) n -> b n l", l=L)
+        D_param_hh = self.D_hh.float().contiguous()
+        delta_bias_hh = self.dt_proj_hh.bias.float().contiguous() if self.dt_proj_hh.bias is not None else None
+
+        h_h = self.selective_scan_with_psi(
+            u=x,
+            psi_seq=psi_seq,
+            dt=dt_hh,
+            A=A_hh,
+            B=B_proj_hh,
+            C=C_proj_hh,
+            D=D_param_hh,
+            psi_proj=self.Psi_proj_hh,
+            delta_bias=delta_bias_hh,
+        )  # [B, C, L]
+
+        h_h_bc = h_h.reshape(B, C, L)
+        x_proj_out_hv = self.x_proj_hv(rearrange(h_h_bc, "b c l -> (b l) c"))
+        dt_inter_hv, B_hv, C_hv = torch.split(x_proj_out_hv, [self.dt_rank, self.state_size, self.state_size], dim=-1)
+        dt_hv = self.dt_proj_hv.weight @ dt_inter_hv.t()
+        dt_hv = rearrange(dt_hv, "c (b l) -> b c l", l=L)
+        A_hv = -torch.exp(self.A_log_hv.float())
+        B_proj_hv = rearrange(B_hv, "(b l) n -> b n l", l=L)
+        C_proj_hv = rearrange(C_hv, "(b l) n -> b n l", l=L)
+        D_param_hv = self.D_hv.float().contiguous()
+        delta_bias_hv = self.dt_proj_hv.bias.float().contiguous() if self.dt_proj_hv.bias is not None else None
+
+        h_v = self.selective_scan_with_psi(
+            u=h_h_bc,
+            psi_seq=psi_seq,
+            dt=dt_hv,
+            A=A_hv,
+            B=B_proj_hv,
+            C=C_proj_hv,
+            D=D_param_hv,
+            psi_proj=self.Psi_proj_hv,
+            delta_bias=delta_bias_hv,
+            x_proj=self.X_proj_hv,
+            x_extra=x,
+        )  # [B, D, L]
+        h_v = h_v.view(B, C, L)
+
+        # output proj
+        y = self.C_h(h_h.permute(0, 2, 1)) + self.C_v(h_v.permute(0, 2, 1))   # [B, C, L]
+        return y.permute(0, 2, 1)
